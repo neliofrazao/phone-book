a6885a4d81c44efd12a77f40f00e01bf18c76a4b
Nélio Frazão <nelio.frazao@invillia.com>
2019-09-03 14:11:36 -0300

    chore(package): add json server

--- src/container/PersonList/PersonList.js
+++ src/container/PersonList/PersonList.js
@@ -3,27 +3,34 @@ import { Paper, Button } from '@material-ui/core'
 import { axios } from '../../util'
 import AppHeader from '../../component/AppHeader/AppHeader';
 import MainTemplate  from '../../template/MainTemplate'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';;
 
 class PersonList extends Component {
   constructor(props){
     super(props)
-    this.state = {
-      listPersonGrid : []
-    }
+    this.state = { listPersonGrid : [] }
   }
-  
-  person = () => {
+
+  getPerson = () => {
     axios.get('phoneList').then( list => {
       this.setState({ listPersonGrid: list.data })
     })
   }
 
-  handleClick = personId => {
+  handlePersonDetail = personId => {
     this.props.history.push(`person-detail/${personId}`)
   }
 
   componentDidMount() {
-    this.person()
+    this.getPerson()
   }
 
   render() {
@@ -31,18 +38,36 @@ class PersonList extends Component {
       <MainTemplate>
         <AppHeader title="Página de listagem" />
          <Paper>
-            <ul>
-                {this.state.listPersonGrid.map(person =>
-                  <li key={person.id}>
-                    Nome: {person.nome} - 
-                    Telefone: {person.telefone} - 
-                    Apelido: {person.apelido}
-                    <Button onClick={() => this.handleClick(person.id)} variant="contained" >
-                      Default
-                    </Button>
-                  </li>
-                )}
-            </ul>
+          <List dense>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <FolderIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Single-line item" />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+          <ul>
+              {this.state.listPersonGrid.map(person =>
+                <li key={person.id}>
+                  Nome: {person.nome} - 
+                  Telefone: {person.telefone} - 
+                  Apelido: {person.apelido}
+                  <Button onClick={() => this.handlePersonDetail(person.id)} variant="contained" >
+                    Default
+                  </Button>
+                </li>
+              )}
+          </ul>
          </Paper>
       </MainTemplate>
     )
