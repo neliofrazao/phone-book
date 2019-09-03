d336dbecac3049868d1ad8fda4dd7b5fba943a85
Nélio Frazão <nelio.frazao@invillia.com>
2019-09-03 11:20:25 -0300

    feat(PersonList): set initial state as empity

--- src/container/PersonList/PersonList.js
+++ src/container/PersonList/PersonList.js
@@ -8,11 +8,7 @@ class PersonList extends Component {
   constructor(props){
     super(props)
     this.state = {
-      listPersonGrid : [
-        { nome: 'Pessoa 1', telefone: '8888-8855', apelido: 'João'},
-        { nome: 'Pessoa 2', telefone: '8888-8844', apelido: 'Maria'},
-        { nome: 'Pessoa 3', telefone: '8888-8833', apelido: 'José'},
-      ]
+      listPersonGrid : []
     }
   }
   
