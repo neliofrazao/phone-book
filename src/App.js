0633e541475bbe11f7c205a26c2fd83506e17ac7
Nélio Frazão <nelio.frazao@invillia.com>
2019-09-03 14:26:41 -0300

    refactor(PhoneBook): change component name PersonList to PhoneBook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import PersonList  from './container/PersonList'
+import PhoneBook  from './container/PhoneBook'
 import PersonDetail from './container/PersonDetail'
 
 const RenderPhoneBook = () => {
   return(
     <Switch>
-      <Route exact path='/' component={PersonList} />
+      <Route exact path='/' component={PhoneBook} />
       <Route exact path='/person-detail/:id' component={PersonDetail} />
     </Switch>
   )
--- src/container/PhoneBook/PhoneBook.js
+++ src/container/PhoneBook/PhoneBook.js
@@ -13,7 +13,7 @@ import IconButton from '@material-ui/core/IconButton';
 import FolderIcon from '@material-ui/icons/Folder';
 import DeleteIcon from '@material-ui/icons/Delete';;
 
-class PersonList extends Component {
+class PhoneBook extends Component {
   constructor(props){
     super(props)
     this.state = { listPersonGrid : [] }
@@ -74,4 +74,4 @@ class PersonList extends Component {
   }
 }
 
-export default PersonList
+export default PhoneBook
