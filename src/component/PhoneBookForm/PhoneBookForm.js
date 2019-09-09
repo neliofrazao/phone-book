e8da6d64c12b0265fb3f9e4b953add6bdacf0320
Nélio Frazão <nelio.frazao@invillia.com>
2019-09-09 17:18:01 -0300

    feat(PhoneBookForm): apply InputField in PhoneBookForm component

--- src/component/PhoneBookForm/PhoneBookForm.js
+++ src/component/PhoneBookForm/PhoneBookForm.js
@@ -1,63 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, Grid, Paper, TextField } from '@material-ui/core'
+import { Button, Grid, Paper } from '@material-ui/core'
 import ActionBar from '../ActionBar'
+import InputField from '../InputField'
 
 const PhoneBookForm = ({ errors, values, handleChange }) => (
   <>
     <Paper className="contact-detail">
       <Grid container justify="center" spacing={3}>
         <Grid item md={12} lg={6}>
-          <TextField
-            fullWidth
-            id="name"
-            label="Nome"
-            name="name"
-            value={values.name || ''}
-            error={Boolean(errors.name)}
-            helperText={errors.name}
-            onChange={handleChange}
-            margin="normal"
-          />
+        <InputField
+          id="name"
+          label="Nome"
+          values={values.name}
+          errors={errors.name}
+          handleChange={handleChange}
+        />
         </Grid>
         <Grid item md={12} lg={6}>
-          <TextField
-            fullWidth
+          <InputField
             id="nickName"
             label="Apelido"
-            name="nickName"
-            value={values.nickName || ''}
-            error={Boolean(errors.nickName)}
-            helperText={errors.nickName}
-            onChange={handleChange}
-            margin="normal"
+            values={values.nickName}
+            errors={errors.nickName}
+            handleChange={handleChange}
           />
         </Grid>
         <Grid item md={12} lg={6}>
-          <TextField
-            fullWidth
+          <InputField
             id="email"
             label="Email"
-            type="email"
-            name="email"
-            value={values.email || ''}
-            error={Boolean(errors.email)}
-            helperText={errors.email}
-            onChange={handleChange}
-            margin="normal"
+            values={values.email}
+            errors={errors.email}
+            handleChange={handleChange}
           />
         </Grid>
         <Grid item md={12} lg={6}>
-          <TextField
-            fullWidth
+          <InputField
             id="phoneNumber"
             label="Telefone"
-            name="phoneNumber"
-            value={values.phoneNumber || ''}
-            error={Boolean(errors.phoneNumber)}
-            helperText={errors.phoneNumber}
-            onChange={handleChange}
-            margin="normal"
+            values={values.phoneNumber}
+            errors={errors.phoneNumber}
+            handleChange={handleChange}
           />
         </Grid>
       </Grid>
