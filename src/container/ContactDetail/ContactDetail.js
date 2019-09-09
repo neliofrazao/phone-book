52d07712e356be18017fcce59fd9ae885d07b5e9
Nélio Frazão <nelio.frazao@invillia.com>
2019-09-09 15:46:50 -0300

    refactor(ContactDetail): remove unnecessary import Main template

--- src/container/ContactDetail/ContactDetail.js
+++ src/container/ContactDetail/ContactDetail.js
@@ -8,7 +8,6 @@ import {
   PhoneAndroidRounded
 } from '@material-ui/icons'
 import AppHeader from '../../component/AppHeader/AppHeader';
-import MainTemplate  from '../../template/MainTemplate'
 import ContactItens from '../../component/ContactItens'
 import Loading from '../../component/Loading'
 import phoneBook from  '../../api/PhoneBook/PhoneBook'
@@ -61,12 +60,12 @@ class ContactDetail extends Component {
     const { id, name, nickName, email, phoneNumber } = this.state.contactDetail
     
     return(
-      <MainTemplate>
+      <>
         {this.state.isLoading ? (
           <Loading />
         ) : (
           <>
-            <AppHeader pageTitle={name}  windowTitle={`${name} ${windowTitle.TITLE}`} >
+            <AppHeader pageTitle={name} windowTitle={`${name} ${windowTitle.TITLE}`} >
               <Button  variant="contained" color="primary" onClick={this.handleNewContact} >
                 Novo Contato
               </Button>
@@ -96,7 +95,7 @@ class ContactDetail extends Component {
             </ActionBar>
           </>
         )}
-      </MainTemplate>
+      </>
     )
   }
 }
