b6d71561e17e0384fc5ce4724cbd27660d0f6842
Nélio Frazão <nelio.frazao@invillia.com>
2019-09-09 15:42:17 -0300

    feat(withMainTemplate): add HOC withMainTemplate

---
+++ src/enhancers/WithMainTemplate/WithMainTemplate.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MainTemplate from '../../template/MainTemplate'
+
+const withMainTemplate = BaseComponent => props =>
+  <MainTemplate component={BaseComponent} {...props} />
+
+export default withMainTemplate
+
