d67769262613e91ffa25e485851bd11c014d1066
Nélio Frazão <nelio.frazao@invillia.com>
2019-09-09 15:45:11 -0300

    refactor(AppPhoneBook): Add HOC WithMainTemplate in AppPhoneBook, and rename App to AppPhoneBook

---
+++ src/AppPhoneBook.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import withMainTemplate from './enhancers/WithMainTemplate/WithMainTemplate'
+import PhoneBook  from './container/PhoneBook'
+import ContactDetail  from './container/ContactDetail'
+import NewContact  from './container/NewContact'
+
+const RenderPhoneBook = () => {
+  return(
+    <Switch>
+      <Route exact path='/' component={PhoneBook} />
+      <Route exact path='/contact-detail/:contactId' component={ContactDetail} />
+      <Route exact path='/new-contact/' component={NewContact} />
+    </Switch>
+  )
+}
+
+const Component = withMainTemplate(RenderPhoneBook)
+
+const AppPhoneBook = props => {
+  return (
+    <Component {...props}/>
+  )
+}
+
+export default AppPhoneBook
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import AppPhoneBook from './AppPhoneBook';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <AppPhoneBook />
   </BrowserRouter>
 , document.getElementById('root'));
 
